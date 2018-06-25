1bfeba02c51fff6ed6a7eb0b762ef12a2810e68d
rodrigojkrs <40372845+rodrigojkrs@users.noreply.github.com>
2018-06-25 19:58:31 -0300

    Update Rodrigo.php

--- Rodrigo.php
+++ Rodrigo.php
@@ -12,6 +12,8 @@ and open the template in the editor.
     <body>
         <h1>Bem vindo ao Site</h1>
         <a href="index.php">Voltar</a>
+        <img src="avatar.png" alt="Texto Alternativo" title="Avatar" />
+        <img src="Arquivo inexistente.jpg" alt="Texto Alternativo" title="Outra figura"/>
         <?php
         // put your code here
         ?>
